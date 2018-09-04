ab34726a3be8ee74ec54d9a86d0ca13cd23c6b34
leeblackc <1013217387@qq.com>
2018-09-04 09:19:24 +0800

    Initial commit

---
+++ src/main/java/edu/zhku/park/dao/MessageMapper.java
@@ -0,0 +1,30 @@
+package edu.zhku.park.dao;
+
+import edu.zhku.park.model.Message;
+import edu.zhku.park.model.MessageExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface MessageMapper {
+    long countByExample(MessageExample example);
+
+    int deleteByExample(MessageExample example);
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Message record);
+
+    int insertSelective(Message record);
+
+    List<Message> selectByExample(MessageExample example);
+
+    Message selectByPrimaryKey(Integer id);
+
+    int updateByExampleSelective(@Param("record") Message record, @Param("example") MessageExample example);
+
+    int updateByExample(@Param("record") Message record, @Param("example") MessageExample example);
+
+    int updateByPrimaryKeySelective(Message record);
+
+    int updateByPrimaryKey(Message record);
+}
---
+++ src/main/java/edu/zhku/park/dao/ParkMapper.java
@@ -0,0 +1,30 @@
+package edu.zhku.park.dao;
+
+import edu.zhku.park.model.Park;
+import edu.zhku.park.model.ParkExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface ParkMapper {
+    long countByExample(ParkExample example);
+
+    int deleteByExample(ParkExample example);
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Park record);
+
+    int insertSelective(Park record);
+
+    List<Park> selectByExample(ParkExample example);
+
+    Park selectByPrimaryKey(Integer id);
+
+    int updateByExampleSelective(@Param("record") Park record, @Param("example") ParkExample example);
+
+    int updateByExample(@Param("record") Park record, @Param("example") ParkExample example);
+
+    int updateByPrimaryKeySelective(Park record);
+
+    int updateByPrimaryKey(Park record);
+}
